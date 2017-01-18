3514033ab07e17b2dc7ad81d7dbc0b120ef9f752
Kevin Guo <kevinguokg@hotmail.com>
2017-01-17 17:03:44 -0800

    initial commit

---
+++ Weather Go/WeatherAPI.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPI.swift
+//  Weather Go
+//
+//  Created by Kevin Guo on 2017-01-17.
+//  Copyright © 2017 Kevin Guo. All rights reserved.
+//
+
+import Foundation
+
+class WeatherAPI {
+    let appKey = "69872ee3396e10cf77e77b72f9ba81ca"
+    
+    static func foo () {
+    
+    }
+}
